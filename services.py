d5ce00f6d41f413882652738c24e1f42d898548c
qgthurier <qgthurier@netbooster.com>
2014-10-24 14:43:41 +0200

    test endpoint psa

--- services.py
+++ services.py
@@ -47,7 +47,7 @@ class PsaBqApi(remote.Service):
         result = (self.bq_service.jobs().query(projectId=BILLING_PROJECT_ID, body=self.make_query_config(query % (request.startDate, request.endDate, request.dealer))).execute())
         #timexec = str(long(result["statistics"]["endTime"]) - long(result["statistics"]["startTime"]))
         logging.debug(result)
-        timexec = 0
+        timexec = str(0)
         values = [str(r['f'][0]["v"]) for r in result['rows']][0]
         return Response(time=timexec, res=values)
           
